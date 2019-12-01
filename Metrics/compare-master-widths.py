a1ec7fe3d27d3a542f9293aef613880e27632e99
Mttymtt <hello@mattymatt.co>
2019-12-01 16:35:41 -0500

    Update compare-master-widths.py

--- Metrics/compare-master-widths.py
+++ Metrics/compare-master-widths.py
@@ -4,11 +4,24 @@ __doc__="""
 Compare widths across font masters of the open font. Outputs a report in Macro Window, and opens Glyphs with mismatched widths in a new tab.
 """
 
+# Need to figure out how to deal with more than two masters
+# Right now it is comparing each master individually to just the first master
+# So it is duplicating information/glyphs
+
+# --------------------
+# Variables
+# --------------------
+
 f = Glyphs.font # open font
+exportableGlyphs = [g for g in f.glyphs if f.export]
 
 # brings macro window to front and clears its log:
 Glyphs.clearLog()
 Glyphs.showMacroWindow()
+
+# --------------------
+# Table
+# --------------------
 
 # make a list of the master names
 # then determine which is the longest
@@ -20,7 +33,7 @@ fontNameLen = len(max(masterNames, key=len)) # this is the longest master name
 # make a list of glyph widths
 # then determine which is the longest unit
 glyphWidths = []
-for g in [g for g in f.glyphs if f.export]:
+for g in exportableGlyphs:
     for i, master in enumerate(f.masters):
         master = f.masters[i]
         masterGlyph = g.layers[master.id]
@@ -31,53 +44,88 @@ widthLen = len(max(glyphWidths, key=len)) # this is the max character length of 
 # make a list of glyph names
 # then determine which is the longest name
 glyphNames = []
-for g in [g for g in f.glyphs if f.export]:
+for g in exportableGlyphs:
     glyphName = g.name
     glyphNames.append(glyphName)
 glyphNameLen = len(max(glyphNames, key=len)) # this is the longest glyph name
 
-lenList = [fontNameLen, widthLen, glyphNameLen]
+# labels for table
+col1Name = "Master"
+col1NameLen = len(col1Name)
 
-# build & label table to display inconsistent glyph widths
-leftColWidth = max(lenList)
-title = "Master".ljust(leftColWidth + 1)
-divider = "".ljust(leftColWidth + 1, "-")
-spacer = "".ljust(widthLen, "-")
-print "%s | wdth" % title
-print "%s | %s" % (divider, spacer)
+col2Name = "Width"
+col2NameLen = len(col2Name)
 
-# make a list of all of the glyphs with inconsistent widths
+# make a list of the longest font name, glyph name, and col1 label
+# then determine which is the longest
+lenList = [fontNameLen, glyphNameLen, col1NameLen]
+col1Width = max(lenList)
+
+# make a list of the longest width len, and col2 label
+# then determine which is the longest
+lenList = [widthLen, col2NameLen]
+col2Width = max(lenList)
+
+# create labels and dividers for the table based on the lengths determined above
+col1 = col1Name.ljust(col1Width + 1)
+col2 = col2Name.ljust(col1Width + 1)
+col1Divider = "".ljust(col1Width + 1, "-")
+col1Spacer = "".ljust(col1Width + 1)
+col2Divider = "".ljust(col2Width, "-")
+
+
+# --------------------
+# Make a list of all of the glyphs with inconsistent widths
+# --------------------
+
 unevenGlyphs = []
 
-for g in [g for g in f.glyphs if f.export]:
+for g in exportableGlyphs:
+    glyphName = g.name
+    glyphWidthList = []
+
+    for i, master in enumerate(f.masters):
+        masterGlyph = g.layers[master.id]
+        masterGlyphWidth = masterGlyph.width
+
+        glyphWidthList.append(masterGlyphWidth)
+    checkWidths = all(x==glyphWidthList[0] for x in glyphWidthList)
+    if checkWidths == False:
+        unevenGlyphs.append(g)
+
+# --------------------
+# Make a table of glyphs with uneven widths across masters
+# --------------------
+
+print "%s | %s\n%s | %s" % (col1, col2, col1Divider, col2Divider)
+
+for g in unevenGlyphs:
     glyphName = g.name
 
-    firstMaster = f.masters[0]
-    firstMasterName = firstMaster.name
-    firstMasterNameFormatted = firstMasterName.ljust(leftColWidth + 1)
-    firstMasterGlyph = g.layers[firstMaster.id]
-    firstMasterGlyphWidth = firstMasterGlyph.width
+    print "%s |\n%s |\n%s |" % (col1Spacer, glyphNameFormatted, col1Spacer)
 
     for i, master in enumerate(f.masters):
         master = f.masters[i]
         masterName = master.name
-        masterNameFormatted = masterName.ljust(leftColWidth + 1)
         masterGlyph = g.layers[master.id]
         masterGlyphWidth = masterGlyph.width
 
-        if firstMasterGlyphWidth != masterGlyphWidth:
-            unevenGlyphs.append(firstMasterGlyph)
+        glyphNameFormatted = glyphName.ljust(col1Width + 1)
+        masterNameFormatted = masterName.ljust(col1Width + 1)
 
-            glyphNameFormatted = glyphName.ljust(leftColWidth + 1)
+        print "%s | %s" % (masterNameFormatted, masterGlyphWidth)
+    print "%s |\n%s | %s" % (col1Spacer, col1Divider, col2Divider)
 
-            print "%s |" % glyphNameFormatted
+# --------------------
+# Open uneven glyphs in new tab
+# --------------------
 
-            print "%s | %s" % (firstMasterNameFormatted, firstMasterGlyphWidth)
-            print "%s | %s" % (masterNameFormatted, masterGlyphWidth)
+newTabGlyphs = []
 
-            # print "%s : %s\n%s : %s" % (firstMaster.name, firstMasterGlyphWidth, masterName.name, masterGlyphWidth)
-            print"%s | %s" % (divider, spacer)
+for g in unevenGlyphs:
+    firstMaster = f.masters[0]
+    firstMasterGlyph = g.layers[firstMaster.id]
+    newTabGlyphs.append(firstMasterGlyph)
 
-if firstMasterGlyphWidth != masterGlyphWidth:
-    newTab = f.newTab()
-    newTab.layers = unevenGlyphs
+newTab = f.newTab()
+newTab.layers = newTabGlyphs
